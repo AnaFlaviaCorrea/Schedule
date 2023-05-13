5fd71219e523ae9ac56bbf129c9754bb38f2e1ba
Ana Flávia Corrêa <anaflaviacorrea@hotmail.com>
2023-05-13 19:06:09 -0300

    "ready"

--- contacts/views.py
+++ contacts/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from . models import Contacts
+
 
 def index(request):
-    return render(request, 'pages/index.html')
+    contacts = Contacts.objects.all()
+    return render(request, 'pages/index.html', {'contacts': contacts})
 
+def search(request):
+    busca = request.GET.get('search')
+   
+    contacts = Contacts.objects.filter(name__icontains = busca)
+    return render(request, 'pages/index.html', {'contacts': contacts})
+    
 
 # HttpResponse('Olá mundo')
